7c0615cbcf66030d97cbae1927cff7d666eb979c
Junio C Hamano <gitster@pobox.com>
2020-04-15 10:28:32 -0700

    SQUASH???

--- refs.c
+++ refs.c
@@ -343,7 +343,6 @@ int ref_filter_match(const char *refname,
 		     const struct string_list *exclude_patterns_config)
 {
 	struct string_list_item *item;
-	int found = 0;
 
 	if (exclude_patterns && exclude_patterns->nr) {
 		for_each_string_list_item(item, exclude_patterns) {
@@ -355,18 +354,13 @@ int ref_filter_match(const char *refname,
 	if (include_patterns && include_patterns->nr) {
 		for_each_string_list_item(item, include_patterns) {
 			if (match_ref_pattern(refname, item)) {
-				found = 1;
-				break;
+				return 1;
 			}
 		}
-
-		if (!found)
-			return 0;
+		return 0;
 	}
 
-	if (!found &&
-	    exclude_patterns_config &&
-	    exclude_patterns_config->nr) {
+	if (exclude_patterns_config && exclude_patterns_config->nr) {
 		for_each_string_list_item(item, exclude_patterns_config) {
 			if (match_ref_pattern(refname, item))
 				return 0;
